2dced11a8baaf31d31a53aeb694d5ddfe84f92b5
Zosha Chung <czosha@gmail.com>
2025-02-16 03:18:42 -0500

    Task 1 - Customer Invoice Calculation

---
+++ cc_7.js
@@ -0,0 +1,12 @@
+// Task 1 Customer Invoice Calculation 
+
+function calculateInvoice(subtotal, taxRate, discount){
+    let total = (subtotal + (subtotal * taxRate)) - discount;
+    console.log(`Total Invoice: $${total.toFixed(2)}`)
+}
+
+// Test Cases 
+
+calculateInvoice(400, 0.05, 7); // Expected output $413
+calculateInvoice(750, 0.2, 4); // Expected output $896
+
