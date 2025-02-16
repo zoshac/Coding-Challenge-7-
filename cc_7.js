2904e7b58a41950e4f41886e30a578f0a83c1b63
Zosha Chung <czosha@gmail.com>
2025-02-16 03:34:15 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -10,3 +10,13 @@ function calculateInvoice(subtotal, taxRate, discount){
 calculateInvoice(400, 0.05, 7); // Expected output $413
 calculateInvoice(750, 0.2, 4); // Expected output $896
 
+// Task 2 Employee Hourly Wage 
+
+const calculateHourlyWage = function(salary, hoursWorkedPerWeek) {
+    let hourlyWage = salary / (hoursWorkedPerWeek * 52); 
+    console.log(`Hourly Wage: $${hourlyWage.toFixed(2)}`);
+}; 
+
+// Test Cases 
+calculateHourlyWage(52000, 40); // Expected output: "Hourly Wage: $25.00"
+calculateHourlyWage(75000, 35); // Expected output: "Hourly Wage: $41.21"
